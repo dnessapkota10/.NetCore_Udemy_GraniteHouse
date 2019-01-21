8c46aeaad54628c5b6287aac5eee4efa943b0a3d
Dinesh Sapkota <Dinesh.Sapkota@JuicePlus.com>
2019-01-21 16:52:07 -0600

    Adding Session in application for Cart Setting and getting value from session Added Cart Icon in nav menu Added Logic to show no of added items to the cart

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using GraniteHouse.Models;
 using GraniteHouse.Data;
 using Microsoft.EntityFrameworkCore;
+using GraniteHouse.Extensions;
 
 namespace GraniteHouse.Controllers
 {
@@ -25,13 +26,29 @@ namespace GraniteHouse.Controllers
             return View(productList);
         }
 
+        //Get : Product Detail
         public async Task<IActionResult> Details(int ? id)
         {
             var product = await _db.Product.Include(m => m.ProductTypes).Where(m=>m.Id==id).FirstOrDefaultAsync(); //included ProductTypes if needed - incase
             return View(product);
         }
+        
+        //Post : Product Detail
+        [HttpPost, ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DetailsPost(int id)
+        {
+            List<int> listShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart"); //Get from Session using Extension method of Extension class SesssionExtensions
+            if (listShoppingCart == null)
+            {
+                listShoppingCart = new List<int>();
+            }
+            listShoppingCart.Add(id);
+            HttpContext.Session.Set("ssShoppingCart", listShoppingCart); //Setting our session variable ssShoppingCart
+            return RedirectToAction("Index", "Home", new { area = "Customer" });
+        }
 
-       
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
