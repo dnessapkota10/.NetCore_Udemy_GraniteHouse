ad7730f1de6b1419fbf4d314b45736a823ff478e
Dinesh Sapkota <Dinesh.Sapkota@JuicePlus.com>
2019-01-22 13:28:13 -0600

    Adding Appointment and ProductSelectedForAppointment models.

---
+++ GraniteHouse/Data/Migrations/20190122192548_addApppointmentAndProductSelectedForAppointment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraniteHouse.Data.Migrations
+{
+    public partial class addApppointmentAndProductSelectedForAppointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppointmentDate = table.Column<DateTime>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: true),
+                    CustomerPhoneNumber = table.Column<string>(nullable: true),
+                    CustomerEmail = table.Column<string>(nullable: true),
+                    IsConfirmed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductSelectedForAppointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppointmentId = table.Column<int>(nullable: false),
+                    AppointmnetId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductSelectedForAppointment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductSelectedForAppointment_Appointment_AppointmnetId",
+                        column: x => x.AppointmnetId,
+                        principalTable: "Appointment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductSelectedForAppointment_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSelectedForAppointment_AppointmnetId",
+                table: "ProductSelectedForAppointment",
+                column: "AppointmnetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSelectedForAppointment_ProductId",
+                table: "ProductSelectedForAppointment",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductSelectedForAppointment");
+
+            migrationBuilder.DropTable(
+                name: "Appointment");
+        }
+    }
+}
---
+++ GraniteHouse/Models/ProductSelectedForAppointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class ProductSelectedForAppointment
+    {
+        public int Id { get; set; }
+
+        public int AppointmentId { get; set; }
+
+        [ForeignKey("AppointmnetId")]
+        public virtual Appointment Appointment { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
